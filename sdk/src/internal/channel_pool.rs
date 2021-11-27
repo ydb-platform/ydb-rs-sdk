6093fd0430364b20e23194eaf654f9570b74a9fa
rekby <rekby@yandex-team.ru>
2021-11-27 04:34:29 +0300

    cache grpc channels

--- sdk/src/internal/channel_pool.rs
+++ sdk/src/internal/channel_pool.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use http::Uri;
 use crate::internal::load_balancer::{LoadBalancer, SharedLoadBalancer};
 use crate::errors::Result;
 use crate::internal::client_common::DBCredentials;
@@ -6,14 +9,27 @@ use crate::internal::grpc::create_grpc_client;
 use crate::internal::middlewares::AuthService;
 
 #[derive(Clone)]
-pub(crate) struct ChannelPool<T> {
+pub(crate) struct ChannelPool<T> where T:Clone{
     create_new_channel_fn: fn (AuthService) -> T,
     credentials: DBCredentials,
     load_balancer: SharedLoadBalancer,
     service: Service,
+    shared_state: Arc<Mutex<SharedState<T>>>
 }
 
-impl<T> ChannelPool<T> {
+struct SharedState<T> {
+    channels: HashMap<Uri,T>,
+}
+
+impl<T> Default for SharedState<T> {
+    fn default() -> Self {
+        return Self{
+            channels: HashMap::new(),
+        }
+    }
+}
+
+impl<T> ChannelPool<T> where T:Clone {
     pub(crate) fn new<CB>(load_balancer: SharedLoadBalancer, credentials: DBCredentials, service: Service, create_new_channel_fn: fn (AuthService) -> T) ->Self
     {
         return Self{
@@ -21,11 +37,31 @@ impl<T> ChannelPool<T> {
             credentials,
             load_balancer,
             service,
+            shared_state: Arc::new(Mutex::new(SharedState::default())),
         }
     }
 
     pub(crate) fn create_channel(&self)->Result<T>{
         let endpoint = self.load_balancer.endpoint(self.service)?;
-        return create_grpc_client(endpoint, self.credentials.clone(), self.create_new_channel_fn);
+        return if let Some(ch) = self.get_channel_from_pool(&endpoint){
+            Ok(ch)
+        } else {
+            match create_grpc_client(endpoint.clone(), self.credentials.clone(), self.create_new_channel_fn) {
+                Ok(ch) => {
+                    self.shared_state.lock()?.channels.insert(endpoint, ch.clone());
+                    Ok(ch)
+                }
+                Err(err)=> Err(err)
+            }
+        };
+    }
+
+    fn get_channel_from_pool(&self, endpoint: &Uri)->Option<T>{
+        return if let Some(ch) = self.shared_state.lock().ok()?.channels.get(endpoint) {
+            println!("got channel from pool for {}", endpoint);
+            Some(ch.clone())
+        } else {
+            None
+        }
     }
 }
