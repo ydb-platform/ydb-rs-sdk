247f44587d05bf2241337f024d13175315cec9e4
rekby <rekby@yandex-team.ru>
2022-02-20 05:08:46 +0300

    sugar init doc

--- sdk/src/sugar.rs
+++ sdk/src/sugar.rs
@@ -1,5 +1,18 @@
 #[macro_export]
 
+/// sugar for manual construct query params
+///
+/// similar to Query::new("SELECT 1").with_params(HashMap::<String, Value>::from_iter(
+///     [
+///         ("$val1".to_string(), 123.into()),
+///         ("$val2".to_string(), "asdas".to_string().into()),
+///     ]
+/// )
+///
+/// Example:
+/// ```no_run
+/// Query::new("SELECT 1").with_params( ydb_params!( "$val1" => 123, "$val2" => "asdas" ))
+/// ```
 macro_rules! ydb_params {
     (
         $($name:expr => $val:expr ),+ $(,)?
