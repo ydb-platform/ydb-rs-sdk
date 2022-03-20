6348960d33b4bfcc7d89d9b5646bf7fd53469cfa
rekby <rekby@yandex-team.ru>
2022-03-20 13:19:26 +0300

    remove global db variable from url example

--- ydb-example-urlshortener/src/ui.rs
+++ ydb-example-urlshortener/src/ui.rs
@@ -4,7 +4,7 @@ use std::convert::Infallible;
 use std::str::FromStr;
 use warp::http::{StatusCode, Uri};
 use warp::{Filter, Reply};
-use ydb::YdbError;
+use ydb::{TableClient, YdbError};
 
 async fn index() -> Result<impl Reply, Infallible> {
     Ok(warp::reply::html(include_str!("index.html")))
@@ -15,10 +15,10 @@ struct GetUrl {
     url: String,
 }
 
-async fn get_url(params: GetUrl) -> Result<impl Reply, Infallible> {
+async fn get_url(table_client: TableClient, params: GetUrl) -> Result<impl Reply, Infallible> {
     let hash = hashers::fnv::fnv1a32(params.url.as_bytes()).to_string();
 
-    match db::insert(hash.clone(), params.url.clone()).await {
+    match db::insert(&table_client, hash.clone(), params.url.clone()).await {
         Ok(_) => Ok(warp::reply::with_status(hash, StatusCode::OK)),
         Err(err) => Ok(warp::reply::with_status(
             format!("failed create short url: {}", err),
@@ -31,8 +31,11 @@ async fn get_url(params: GetUrl) -> Result<impl Reply, Infallible> {
 struct RedirectParams {
     l: String,
 }
-async fn redirect(params: RedirectParams) -> Result<impl Reply, Infallible> {
-    let reply: Box<dyn Reply> = match db::get(params.l).await {
+async fn redirect(
+    table_client: TableClient,
+    params: RedirectParams,
+) -> Result<impl Reply, Infallible> {
+    let reply: Box<dyn Reply> = match db::get(&table_client, params.l).await {
         Ok(url) => match Uri::from_str(url.as_str()) {
             Ok(uri) => Box::new(warp::redirect::redirect(uri).into_response()),
             Err(err) => Box::new(warp::reply::with_status(
@@ -52,13 +55,25 @@ async fn redirect(params: RedirectParams) -> Result<impl Reply, Infallible> {
     return Ok(reply);
 }
 
-pub async fn run() -> Result<(), warp::Error> {
+fn with_db(
+    table_client: TableClient,
+) -> impl Filter<Extract = (TableClient,), Error = Infallible> + Clone {
+    warp::any().map(move || table_client.clone())
+}
+
+pub async fn run(table_client: TableClient) -> Result<(), warp::Error> {
     // GET /[?l=][?url=]
     let index = warp::get().and_then(index);
 
-    let url = warp::get().and(warp::query()).and_then(get_url);
+    let url = warp::get()
+        .and(with_db(table_client.clone()))
+        .and(warp::query())
+        .and_then(get_url);
 
-    let redirect_page = warp::get().and(warp::query()).and_then(redirect);
+    let redirect_page = warp::get()
+        .and(with_db(table_client.clone()))
+        .and(warp::query())
+        .and_then(redirect);
 
     warp::serve(url.or(redirect_page).or(index))
         .run(([127, 0, 0, 1], 8000))
