f64b1bfb6ed282abd6fe1f631acd81f773cf1afd
robot <timofey.koolin@gmail.com>
2022-06-18 18:38:21 +0300

    fix directory client test - use database from credentials instead of hardcoded /local.

--- ydb/src/client_directory_test_integration.rs
+++ ydb/src/client_directory_test_integration.rs
@@ -11,17 +11,18 @@ use crate::test_integration_helper::create_client;
 #[ignore] // need YDB access
 async fn create_list_remove_directory() -> YdbResult<()> {
     let client = create_client().await?;
+    let database_path = client.database();
     let mut scheme_client = client.scheme_client();
     let time_now = time::SystemTime::now().duration_since(UNIX_EPOCH)?;
     let directory_name = format!("directory_{}", time_now.as_millis());
-    let directory_path = format!("local/{}", directory_name.clone());
+    let directory_path = format!("{}/{}", database_path, directory_name.clone());
 
     scheme_client.make_directory(directory_path.clone()).await?;
-    let directories = scheme_client.list_directory("local/".to_owned()).await?;
+    let directories = scheme_client.list_directory(database_path.clone()).await?;
     assert!(directories.iter().any(|d| d.name == directory_name));
 
     scheme_client.remove_directory(directory_path).await?;
-    let directories = scheme_client.list_directory("local/".to_owned()).await?;
+    let directories = scheme_client.list_directory(database_path).await?;
     assert!(!directories.iter().any(|d| d.name == directory_name));
 
     Ok(())
