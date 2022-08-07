0be1fa1e93e4a30fb8db872afc6d2de0025615b4
Timofey Koolin <timofey.koolin@gmail.com>
2022-08-07 20:42:40 +0300

    tmp allow dead code

--- ydb/src/grpc_wrapper/mod.rs
+++ ydb/src/grpc_wrapper/mod.rs
@@ -9,6 +9,13 @@ pub(crate) mod raw_discovery_client;
 pub(crate) mod raw_errors;
 pub(crate) mod raw_scheme_client;
 pub(crate) mod raw_services;
+
+// tmp, need to implement and remove allow
+#[allow(dead_code)]
 pub(crate) mod raw_status;
+
+// tmp, need to implement and remove allow
+#[allow(dead_code)]
 pub(crate) mod raw_table_service;
+
 pub(crate) mod raw_ydb_operation;
