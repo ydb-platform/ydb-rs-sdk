0a10340ea044ad4a2f95988de33d42742a25cf60
Timofey Koolin <timofey.koolin@gmail.com>
2022-08-12 23:02:59 +0300

    fix linter

--- ydb/src/trace_helpers.rs
+++ ydb/src/trace_helpers.rs
@@ -6,6 +6,6 @@ pub(crate) fn ensure_len_string(s: String) -> String {
     if s.len() <= TRACE_VALUE_MAX_LEN {
         s
     } else {
-        s.chars().take(TRACE_VALUE_MAX_LEN).join("").to_string() + "..."
+        s.chars().take(TRACE_VALUE_MAX_LEN).join("") + "..."
     }
 }
