6c920a3d40c667dba160ea48ba79a95b03a6da2b
Timofey Koolin <timofey.koolin@gmail.com>
2022-08-13 02:03:40 +0300

    add test for parse database in old and new style

--- ydb/src/test_helpers.rs
+++ ydb/src/test_helpers.rs
@@ -3,7 +3,7 @@ use once_cell::sync::Lazy;
 
 pub(crate) static CONNECTION_STRING: Lazy<String> = Lazy::new(|| {
     std::env::var("YDB_CONNECTION_STRING")
-        .unwrap_or_else(|_| "grpc://localhost:2136?database=/local".to_string())
+        .unwrap_or_else(|_| "grpc://localhost:2136/local".to_string())
         .parse()
         .unwrap()
 });
