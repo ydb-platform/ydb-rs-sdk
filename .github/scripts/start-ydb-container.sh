ff02328b83c52dd7531503a37197236dd17378b4
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2022-12-20 17:13:28 +0300

    Update start-ydb-container.sh

--- .github/scripts/start-ydb-container.sh
+++ .github/scripts/start-ydb-container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker run -d --rm --name ydb-local -h localhost -p 2136:2136 -e YDB_USE_IN_MEMORY_PDISKS=false amyasnikov/ydb:slim
+docker run -d --rm --name ydb-local -h localhost -p 2136:2136 -e YDB_USE_IN_MEMORY_PDISKS=false amyasnikov/ydb:latest
 
-while ! docker run --network host amyasnikov/ydb:slim /ydb -e grpc://localhost:2136 -d /local scheme ls; do
+while ! docker run --network host ydb-local /ydb -e grpc://localhost:2136 -d /local scheme ls; do
   echo wait db...
   sleep 3
 done
