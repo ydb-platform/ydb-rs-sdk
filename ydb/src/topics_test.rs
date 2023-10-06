a61e85be23fb1069f85753ea32b35ec3343b7132
Timofey Koolin <rekby@yandex-team.com>
2023-10-07 00:08:33 +0300

    init test message reader

--- ydb/src/topics_test.rs
+++ ydb/src/topics_test.rs
@@ -1,14 +1,14 @@
+use futures_util::StreamExt;
 use tracing_test::traced_test;
 
 use crate::client_topic::list_types::ConsumerBuilder;
 use crate::test_integration_helper::create_client;
-use crate::{
-    client_topic::client::TopicOptionsBuilder, TopicWriterMessageBuilder,
-    TopicWriterOptionsBuilder, YdbResult,
-};
-use tracing::trace;
+use crate::{client_topic::client::TopicOptionsBuilder, TopicWriterMessageBuilder, TopicWriterOptionsBuilder, YdbError, YdbResult};
+use tracing::{trace, warn};
 use ydb_grpc::ydb_proto::topic::stream_read_message::init_request::TopicReadSettings;
 use ydb_grpc::ydb_proto::topic::{stream_read_message, stream_write_message};
+use ydb_grpc::ydb_proto::topic::v1::topic_service_client::TopicServiceClient;
+use crate::grpc_wrapper::runtime_interceptors::InterceptedChannel;
 
 #[tokio::test]
 #[traced_test]
@@ -124,10 +124,128 @@ async fn send_message_test() -> YdbResult<()> {
     println!("sent-4");
 
     // TODO: read messages with raw grpc queries to check it;
-    let mut _grpc_client = topic_client
+    let mut grpc_client = topic_client
         .raw_client_connection()
         .await?
         .get_grpc_service();
 
+    let mut topic_messages = start_read_topic(
+        grpc_client,
+        consumer_name,
+        topic_path,
+    ).await?;
+
+    println!("message: {:?}", topic_messages.recv().await);
+
     Ok(())
+}
+
+async fn start_read_topic(
+    mut grpc_topic_service: TopicServiceClient<InterceptedChannel>,
+    consumer: String,
+    topic_path: String,
+)->YdbResult<tokio::sync::mpsc::UnboundedReceiver<stream_read_message::read_response::MessageData>>{
+    let (reader_stream_tx, reader_stream_rx): (
+        tokio::sync::mpsc::UnboundedSender<stream_read_message::FromClient>,
+        tokio::sync::mpsc::UnboundedReceiver<stream_read_message::FromClient>,
+    ) = tokio::sync::mpsc::unbounded_channel();
+
+    let init_request = stream_read_message::from_client::ClientMessage::InitRequest(
+        stream_read_message::InitRequest{
+            topics_read_settings: vec![
+                TopicReadSettings{
+                    path: topic_path,
+                    ..TopicReadSettings::default()
+                }
+            ],
+            consumer,
+            ..stream_read_message::InitRequest::default()
+        }
+    );
+
+    let mess = stream_read_message::FromClient{
+        client_message: Some(init_request),
+    };
+
+    reader_stream_tx.send(mess).expect("failed to send init message from test topic reader");
+
+
+    let request_stream = tokio_stream::wrappers::UnboundedReceiverStream::new(reader_stream_rx);
+    let mut reader_stream = grpc_topic_service.stream_read(request_stream).await?.into_inner();
+
+    let _init_response = reader_stream.next().await.ok_or(YdbError::custom("failed receive init response in test reader"))??;
+
+    let data_request = stream_read_message::from_client::ClientMessage::ReadRequest(
+        stream_read_message::ReadRequest{
+            bytes_size: 1024*1024,
+            ..stream_read_message::ReadRequest::default()
+        }
+    );
+
+    let mess = stream_read_message::FromClient{
+        client_message: Some(data_request),
+    };
+    reader_stream_tx.send(mess).expect("failed to send data request in test topic reader");
+
+    let (topic_messages_tx, topic_messages_rx): (
+        tokio::sync::mpsc::UnboundedSender<stream_read_message::read_response::MessageData>,
+        tokio::sync::mpsc::UnboundedReceiver<stream_read_message::read_response::MessageData>,
+    ) = tokio::sync::mpsc::unbounded_channel();
+
+
+    tokio::spawn(async move {
+        loop {
+            let mess = reader_stream.next().await;
+            let mess = match mess {
+                Some(Ok(mess)) => mess,
+                mess => {
+                    trace!("stop to receive reader stream mess in test: {:?}", mess);
+                    return
+                }
+            };
+
+            let mess = if let stream_read_message::FromServer{server_message: Some(mess), ..} = mess {
+                mess
+            } else {
+                warn!("failed decode server message in test topic reader: {:?}", mess);
+                return
+            };
+
+            match mess {
+                stream_read_message::from_server::ServerMessage::StartPartitionSessionRequest(
+                    stream_read_message::StartPartitionSessionRequest{
+                        partition_session: Some(partition_session),
+                        ..
+                    }
+                ) => {
+                    reader_stream_tx.send(stream_read_message::FromClient{ client_message: Some(
+                        stream_read_message::from_client::ClientMessage::StartPartitionSessionResponse(
+                            stream_read_message::StartPartitionSessionResponse{
+                                partition_session_id: partition_session.partition_session_id,
+                                ..stream_read_message::StartPartitionSessionResponse::default()
+                            }
+                        )) }).expect("send start partition response in test topic reader")
+                },
+                stream_read_message::from_server::ServerMessage::ReadResponse(
+                    stream_read_message::ReadResponse{
+                       partition_data,
+                        ..
+                    }
+                ) => {
+                    for pd in partition_data.into_iter(){
+                        for batch in pd.batches.into_iter(){
+                            for message in batch.message_data {
+                                topic_messages_tx.send(message).expect("failed to send message from test topic reader")
+                            }
+                        }
+                    }
+                }
+                mess => {
+                    trace!("skip message in test reader: {:?}", mess);
+                }
+            };
+        }
+    });
+
+    Ok(topic_messages_rx)
 }
