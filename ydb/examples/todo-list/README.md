b891e0136d8be0145b8058b4b0c51e5da8d17db8
Rinat Shigapov <rinatshigapov@gmail.com>
2023-12-08 14:25:15 +0300

    todo-list: add initdb/cleardb commands, cleanup

--- ydb/examples/todo-list/README.md
+++ ydb/examples/todo-list/README.md
@@ -1,11 +1,15 @@
 # Prerequisites
 
-Ydb is available at `grpc://localhost:2136?database=local`
+Ydb is available at `grpc://localhost:2136?database=local`. Custom connection string could be specified via `YDB_CONNECTION_STRING` env variable.
+
+# Initialize todo list database
+
+cargo run -p todo-list initdb
 
 # Add todo item
 
-cargo run add 1 my todo item
+cargo run -p todo-list add 1 my todo item
 
 # List todo items
 
-cargo run list
+cargo run -p todo-list list
--- ydb/examples/todo-list/src/main.rs
+++ ydb/examples/todo-list/src/main.rs
@@ -1,18 +1,24 @@
-use std::str::{FromStr};
-use std::num::ParseIntError;
-use std::fmt::{self, Debug};
-use ydb::{ClientBuilder, Query, TableClient, StaticToken, ydb_params, YdbError, YdbOrCustomerError};
 use std::env;
+use std::fmt::{self, Debug};
+use std::num::ParseIntError;
+use std::str::FromStr;
+use ydb::{
+    ydb_params, ClientBuilder, Query, StaticToken, TableClient, YdbError, YdbOrCustomerError,
+};
 
-const USAGE: &'static str = "
+const USAGE: &str = "
 Usage: <command> <cmd_args>
 
-commands:
+Available commands:
 
+initdb
+    Initialises todo list database
 add <id> my todo
+    Adds a todo item with the provided id
 list
-done <todo_id>
-delete <todo_id>
+    Prints upto 200 todo items
+cleardb
+    Deinitializes todo list database
 ";
 
 enum TodoListError {
@@ -50,22 +56,23 @@ impl Debug for TodoListError {
             Self::YdbOrCustomer(ydb_err) => write!(f, "{}", ydb_err),
             Self::IdRequired => write!(f, "argument id is required"),
             Self::ParseInt(err) => write!(f, "id is a nonnegative integer: {}", err),
-            Self::TextRequired => write!(f, "argument text is required")
+            Self::TextRequired => write!(f, "argument text is required"),
         }
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), TodoListError> {
-
     // create the driver
-    let client = ClientBuilder::from_str("grpc://localhost:2136?database=local")?
-    .with_credentials(StaticToken::from("asd"))
-    .client()?;
+    let conn_string = std::env::var("YDB_CONNECTION_STRING")
+        .unwrap_or_else(|_| "grpc://localhost:2136?database=/local".to_string());
+    let client = ClientBuilder::from_str(&conn_string)?
+        .with_credentials(StaticToken::from("asd"))
+        .client()?;
 
     // wait until the background initialization of the driver finishes
     client.wait().await?;
-    
+
     let mut args = env::args().skip(1);
     let cmd = args.next().ok_or_else(|| TodoListError::Usage)?;
 
@@ -74,13 +81,21 @@ async fn main() -> Result<(), TodoListError> {
             let todo = parse_add(args)?;
             let mut table_client = client.table_client(); // create table client
             add_todo(&mut table_client, todo).await?;
-        },
+        }
         "list" => {
             let mut table_client = client.table_client(); // create table client
             list(&mut table_client).await?
-        },
-        "done" => println!("done"),
-        "delete" => println!("delete"),
+        }
+        "initdb" => {
+            let mut table_client = client.table_client(); // create table client
+            init_db(&mut table_client).await?
+        }
+        "cleardb" => {
+            let mut table_client = client.table_client(); // create table client
+            clear_db(&mut table_client).await?
+        }
+        // "done" => println!("done"),
+        // "delete" => println!("delete"),
         _ => {
             eprintln!("{}", USAGE);
             std::process::exit(1);
@@ -93,10 +108,13 @@ async fn main() -> Result<(), TodoListError> {
 struct TodoItem {
     id: u64,
     text: String,
-    _done: bool
+    _done: bool,
 }
 
-fn parse_add<I>(mut args: I) -> Result<TodoItem, TodoListError> where I: Iterator<Item=String> + ExactSizeIterator {
+fn parse_add<I>(mut args: I) -> Result<TodoItem, TodoListError>
+where
+    I: Iterator<Item = String> + ExactSizeIterator,
+{
     let id_raw = args.next().ok_or_else(|| TodoListError::IdRequired)?;
     let id = id_raw.parse::<u64>()?;
     if args.len() == 0 {
@@ -106,38 +124,34 @@ fn parse_add<I>(mut args: I) -> Result<TodoItem, TodoListError> where I: Iterato
     Ok(TodoItem {
         id,
         text: arguments.join(" "),
-        _done: false
+        _done: false,
     })
 }
 
 async fn add_todo(table_client: &mut TableClient, todo: TodoItem) -> Result<(), TodoListError> {
     let text = todo.text.as_str();
-    // read the query result
-    // let table_client = table_client.clone_with_transaction_options(
-    //     ydb::TransactionOptions::new()
-    //         .with_autocommit(true)
-    // );
-    table_client.retry_transaction(|mut tx| async move {
-        // the code in transaction can retry a few times if there was a retriable error
-        tx.query(
-            ydb::Query::from(
-                "
-DECLARE $id as UInt64;
-DECLARE $text as Utf8;
+    table_client
+        .retry_transaction(|mut tx| async move {
+            // the code in transaction can retry a few times if there was a retriable error
+            tx.query(
+                ydb::Query::from(
+                    "
+                    DECLARE $id as UInt64;
+                    DECLARE $text as Utf8;
 
-INSERT INTO todo_items
-     (id, item, done)
-VALUES
-    ($id, $text, false);
-",
+                    INSERT INTO todo_items
+                        (id, item, done)
+                    VALUES
+                        ($id, $text, false);
+                ",
+                )
+                .with_params(ydb_params!("$id" => todo.id, "$text" => text)),
             )
-            .with_params(ydb_params!("$id" => todo.id, "$text" => text)),
-        )
+            .await?;
+            tx.commit().await?;
+            Ok(())
+        })
         .await?;
-        tx.commit().await?;
-        Ok(())
-    })
-    .await?;
     Ok(())
 }
 
@@ -151,17 +165,15 @@ async fn list(table_client: &mut TableClient) -> Result<(), TodoListError> {
         .retry_transaction(|tx| async {
             let mut tx = tx; // move tx lifetime into code block
             let rows = tx
-                .query(
-                    Query::from(
-       "
-			    SELECT
-				    id, item, done
-			    FROM
-                    todo_items
-                LIMIT 200;
-        ",
-                    )
-                )
+                .query(Query::from(
+                    "
+                    SELECT
+                        id, item, done
+                    FROM
+                        todo_items
+                    LIMIT 200;
+                ",
+                ))
                 .await?
                 .into_only_result()?
                 .rows();
@@ -171,32 +183,38 @@ async fn list(table_client: &mut TableClient) -> Result<(), TodoListError> {
                 let text: Option<String> = row.remove_field_by_name("item")?.try_into()?;
                 let done: Option<bool> = row.remove_field_by_name("done")?.try_into()?;
 
-                println!("{}\t{}\t{}", id.unwrap(), text.unwrap(), done.unwrap());
+                println!(
+                    "{}\t{}\t{}",
+                    id.expect("not null"),
+                    text.expect("not null"),
+                    done.expect("not null")
+                );
             }
             Ok(())
         })
         .await?;
-        Ok(())
+    Ok(())
 }
 
-// pub async fn init_db() -> ydb::YdbResult<ydb::Client> {
-//     let conn_string = std::env::var("YDB_CONNECTION_STRING")
-//         .unwrap_or_else(|_| "grpc://localhost:2136?database=/local".to_string());
-//     let client = ydb::ClientBuilder::new_from_connection_string(conn_string)?.client()?;
+async fn init_db(table_client: &mut TableClient) -> Result<(), TodoListError> {
+    table_client
+        .retry_execute_scheme_query(
+            "CREATE TABLE todo_items (
+                id UInt64,
+                item Utf8,
+                done Bool,
+                PRIMARY KEY (id)
+            );
+",
+        )
+        .await?;
 
-//     client.wait().await?;
+    Ok(())
+}
 
-//     let table_client = client.table_client();
-//     table_client
-//         .retry_execute_scheme_query(
-//             "CREATE TABLE  (
-//                     src Utf8,
-// 				    hash Utf8,
-// 				    PRIMARY KEY (hash)
-// 			    );
-// ",
-//         )
-//         .await?;
-
-//     Ok(client)
-// }
+async fn clear_db(table_client: &mut TableClient) -> Result<(), TodoListError> {
+    table_client
+        .retry_execute_scheme_query("DROP TABLE todo_items;")
+        .await?;
+    Ok(())
+}
