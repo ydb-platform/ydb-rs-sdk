0b1ab97bf21711e03e193b2373b84a57dc5d003e
uslon <uslonp18@yandex.ru>
2024-05-17 11:28:29 +0300

    use old credentials constructor correctly

--- ydb/examples/auth-static-credentials.rs
+++ ydb/examples/auth-static-credentials.rs
@@ -11,7 +11,6 @@ async fn main() -> YdbResult<()> {
                 "1234".to_string(),
                 http::uri::Uri::from_static("grpc://localhost:2136/local"),
                 "local".to_string(),
-                None,
             ))
             .client()?;
     client.wait().await?;
