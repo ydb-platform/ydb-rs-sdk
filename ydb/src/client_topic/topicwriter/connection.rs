48236a4d7259a4ae0a1ccfd066f8bf3316ba0e45
sultanowskii <dsr431@yandex.ru>
2026-02-28 17:13:04 +0300

    extract stream writer into a separate file

---
+++ ydb/src/client_topic/topicwriter/connection.rs
@@ -0,0 +1,9 @@
+use crate::grpc_wrapper::raw_topic_service::common::codecs::RawSupportedCodecs;
+
+#[derive(Clone, Debug)]
+pub(crate) struct ConnectionInfo {
+    pub(crate) partition_id: i64,
+    pub(crate) session_id: String,
+    pub(crate) last_seq_no_assigned: i64,
+    pub(crate) codecs_from_server: RawSupportedCodecs,
+}
