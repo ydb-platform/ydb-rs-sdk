a9140aeeda3d6b24158d0351a0cab62ea9aa921b
sultanowskii <dsr431@yandex.ru>
2026-03-15 16:45:54 +0300

    events-based wait/get_messages, various fixes

--- ydb/src/client_topic/topicwriter/message_queue.rs
+++ ydb/src/client_topic/topicwriter/message_queue.rs
@@ -1,10 +1,132 @@
-use std::collections::VecDeque;
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+use tokio::{
+    sync::{mpsc, Mutex as TokioMutex, Notify},
+    time::timeout,
+};
+use tracing::log::trace;
 
 use ydb_grpc::ydb_proto::topic::stream_write_message::write_request::MessageData;
 
 use crate::{YdbError, YdbResult};
 
+#[derive(Clone)]
 pub(crate) struct MessageQueue {
+    inner: Arc<Mutex<MessageQueueInner>>,
+
+    new_message_added: Arc<Notify>,
+    message_acknowledged_rx: Arc<TokioMutex<mpsc::UnboundedReceiver<()>>>,
+}
+
+impl MessageQueue {
+    pub(crate) fn new() -> Self {
+        let new_message_added = Arc::new(Notify::new());
+        let (message_acknowledged_tx, message_acknowledged_rx) = mpsc::unbounded_channel();
+
+        Self {
+            inner: Arc::new(Mutex::new(MessageQueueInner::new(
+                new_message_added.clone(),
+                message_acknowledged_tx,
+            ))),
+            new_message_added,
+            message_acknowledged_rx: Arc::new(TokioMutex::new(message_acknowledged_rx)),
+        }
+    }
+
+    pub(crate) fn add_message(&self, message: MessageData) -> YdbResult<()> {
+        let mut inner = self.inner.lock().unwrap();
+        inner.add_message(message)
+    }
+
+    fn get_messages_to_send_without_threshold(&self) -> YdbResult<Vec<MessageData>> {
+        let mut inner = self.inner.lock().unwrap();
+        inner.get_messages_to_send()
+    }
+
+    fn get_messages_to_send_with_length_threshold(
+        &self,
+        length_threshold: usize,
+    ) -> GetMessagesToSendResult {
+        let mut inner = self.inner.lock().unwrap();
+        inner.get_messages_to_send_with_length_threshold(length_threshold)
+    }
+
+    async fn get_messages_to_send_loop(&self, length_threshold: usize) -> GetMessagesToSendResult {
+        loop {
+            tokio::select! {
+                _ = self.new_message_added.notified() => {
+                    match self.get_messages_to_send_with_length_threshold(length_threshold) {
+                        success @ GetMessagesToSendResult::Ok(_) => return success,
+                        GetMessagesToSendResult::NotEnoughMessages => continue,
+                        error @ GetMessagesToSendResult::Err(_) => return error,
+                    }
+                }
+            }
+        }
+    }
+
+    pub(crate) async fn get_messages_to_send(
+        &self,
+        length_threshold: usize,
+        duration: Duration,
+    ) -> GetMessagesToSendResult {
+        match timeout(duration, self.get_messages_to_send_loop(length_threshold)).await {
+            Ok(result) => result,
+            Err(_) => match self.get_messages_to_send_without_threshold() {
+                Ok(messages) => GetMessagesToSendResult::Ok(messages),
+                Err(error) => GetMessagesToSendResult::Err(error),
+            },
+        }
+    }
+
+    pub(crate) fn acknowledge_message(&self, seq_no: i64) -> YdbResult<()> {
+        let mut inner = self.inner.lock().unwrap();
+        inner.acknowledge_message(seq_no)
+    }
+
+    pub(crate) fn reset_progress(&self) {
+        let mut inner = self.inner.lock().unwrap();
+        inner.reset_progress()
+    }
+
+    pub(crate) fn close_for_new_messages(&self) {
+        let mut inner = self.inner.lock().unwrap();
+        inner.close_for_new_messages()
+    }
+
+    fn is_empty(&self) -> bool {
+        let inner = self.inner.lock().unwrap();
+        inner.is_empty()
+    }
+
+    pub(crate) async fn wait(&self) {
+        if self.is_empty() {
+            return;
+        }
+
+        let mut message_acknowledged_rx = self.message_acknowledged_rx.lock().await;
+
+        loop {
+            tokio::select! {
+                Some(_) = message_acknowledged_rx.recv() => {
+                    if self.is_empty() {
+                        break;
+                    }
+                }
+                else => {
+                    trace!("message acknowledged channel is closed");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+struct MessageQueueInner {
     messages: VecDeque<MessageData>,
 
     // index of the last message that has been added to the queue
@@ -15,10 +137,13 @@ pub(crate) struct MessageQueue {
     last_sent_index: Option<usize>,
 
     is_open_for_new_messages: bool,
+
+    new_message_added: Arc<Notify>,
+    message_acknowledged_tx: mpsc::UnboundedSender<()>,
 }
 
 #[derive(Debug)]
-pub(crate) enum GetMessagesToSendWithLengthThresholdResult {
+pub(crate) enum GetMessagesToSendResult {
     Ok(Vec<MessageData>),
     // TODO: In the future, when the batch is checked by the serialized size (not the length), this
     // variant has to be changed accordingly.
@@ -27,18 +152,23 @@ pub(crate) enum GetMessagesToSendWithLengthThresholdResult {
 }
 
 // TODO: add a method to wait for all messages to be sent and use this method in stop() / flush()
-impl MessageQueue {
-    pub(crate) fn new() -> Self {
+impl MessageQueueInner {
+    pub(crate) fn new(
+        new_message_added: Arc<Notify>,
+        message_acknowledged_tx: mpsc::UnboundedSender<()>,
+    ) -> Self {
         Self {
             messages: VecDeque::new(),
             last_written_index: None,
             last_written_seq_no: -1,
             last_sent_index: None,
             is_open_for_new_messages: true,
+            new_message_added,
+            message_acknowledged_tx,
         }
     }
 
-    pub(crate) fn add_message(&mut self, message: MessageData) -> YdbResult<()> {
+    fn add_message(&mut self, message: MessageData) -> YdbResult<()> {
         if !self.is_open_for_new_messages {
             return Err(YdbError::Custom(
                 "message queue is closed for new messages".to_string(),
@@ -53,6 +183,8 @@ impl MessageQueue {
 
         self.messages.push_back(message);
 
+        self.new_message_added.notify_waiters();
+
         Ok(())
     }
 
@@ -66,7 +198,7 @@ impl MessageQueue {
         Ok(())
     }
 
-    pub(crate) fn get_length_of_messages_to_send(&self) -> YdbResult<usize> {
+    fn get_length_of_messages_to_send(&self) -> YdbResult<usize> {
         match (self.last_written_index, self.last_sent_index) {
             (None, None) => Ok(0),
             (Some(written_idx), None) => Ok(written_idx + 1),
@@ -94,37 +226,37 @@ impl MessageQueue {
         result
     }
 
-    pub(crate) fn get_messages_to_send(&mut self) -> YdbResult<Vec<MessageData>> {
+    fn get_messages_to_send(&mut self) -> YdbResult<Vec<MessageData>> {
         let length = self.get_length_of_messages_to_send()?;
 
-        Ok(MessageQueue::do_get_messages_to_send(
+        Ok(MessageQueueInner::do_get_messages_to_send(
             &self.messages,
             &mut self.last_sent_index,
             length,
         ))
     }
 
-    pub(crate) fn get_messages_to_send_with_length_threshold(
+    fn get_messages_to_send_with_length_threshold(
         &mut self,
         length_threshold: usize,
-    ) -> GetMessagesToSendWithLengthThresholdResult {
+    ) -> GetMessagesToSendResult {
         let length = match self.get_length_of_messages_to_send() {
             Ok(length) => length,
-            Err(error) => return GetMessagesToSendWithLengthThresholdResult::Err(error),
+            Err(error) => return GetMessagesToSendResult::Err(error),
         };
 
         if length < length_threshold {
-            return GetMessagesToSendWithLengthThresholdResult::NotEnoughMessages;
+            return GetMessagesToSendResult::NotEnoughMessages;
         }
 
-        GetMessagesToSendWithLengthThresholdResult::Ok(MessageQueue::do_get_messages_to_send(
+        GetMessagesToSendResult::Ok(MessageQueueInner::do_get_messages_to_send(
             &self.messages,
             &mut self.last_sent_index,
             length,
         ))
     }
 
-    pub(crate) fn acknowledge_message(&mut self, seq_no: i64) -> YdbResult<()> {
+    fn acknowledge_message(&mut self, seq_no: i64) -> YdbResult<()> {
         let Some(message) = self.messages.front() else {
             return Err(YdbError::Custom(format!(
                 "ack unexpected message with seq_no={}",
@@ -149,15 +281,21 @@ impl MessageQueue {
             _ => None,
         };
 
+        self.message_acknowledged_tx.send(()).unwrap();
+
         Ok(())
     }
 
-    pub(crate) fn reset_progress(&mut self) {
+    fn reset_progress(&mut self) {
         self.last_sent_index = None;
     }
 
-    pub(crate) fn close_for_new_messages(&mut self) {
+    fn close_for_new_messages(&mut self) {
         self.is_open_for_new_messages = false;
+    }
+
+    fn is_empty(&self) -> bool {
+        self.messages.is_empty()
     }
 }
 
@@ -177,9 +315,15 @@ mod tests {
         }
     }
 
+    fn create_queue() -> MessageQueueInner {
+        let new_message_added = Arc::new(Notify::new());
+        let (message_acknowledged_tx, _) = mpsc::unbounded_channel();
+        MessageQueueInner::new(new_message_added, message_acknowledged_tx)
+    }
+
     #[test]
     fn new_creates_empty_queue() {
-        let q = MessageQueue::new();
+        let q = create_queue();
         assert_eq!(q.last_written_index, None);
         assert_eq!(q.last_written_seq_no, -1);
         assert_eq!(q.last_sent_index, None);
@@ -188,7 +332,7 @@ mod tests {
 
     #[test]
     fn get_length_of_messages_to_send_empty() {
-        let q = MessageQueue::new();
+        let q = create_queue();
         match q.get_length_of_messages_to_send() {
             Ok(length) => assert_eq!(length, 0),
             Err(error) => panic!("{}", error.to_string()),
@@ -197,7 +341,7 @@ mod tests {
 
     #[test]
     fn get_length_of_messages_to_send_with_some_added_and_none_sent() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.last_written_index = Some(4);
         q.last_sent_index = None;
         assert_eq!(q.get_length_of_messages_to_send().unwrap(), 5);
@@ -205,7 +349,7 @@ mod tests {
 
     #[test]
     fn get_length_of_messages_to_send_with_some_added_and_one_sent() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.last_written_index = Some(4);
         q.last_sent_index = Some(0);
         assert_eq!(q.get_length_of_messages_to_send().unwrap(), 4);
@@ -213,7 +357,7 @@ mod tests {
 
     #[test]
     fn get_length_of_messages_to_send_with_some_added_and_same_sent() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.last_written_index = Some(4);
         q.last_sent_index = Some(4);
         assert_eq!(q.get_length_of_messages_to_send().unwrap(), 0);
@@ -221,7 +365,7 @@ mod tests {
 
     #[test]
     fn get_length_of_messages_to_send_with_none_added_and_some_sent() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.last_written_index = None;
         q.last_sent_index = Some(4);
         let err = q.get_length_of_messages_to_send().unwrap_err();
@@ -232,7 +376,7 @@ mod tests {
 
     #[test]
     fn add_message_appends_and_updates_fields() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(10, vec![1, 2, 3])).unwrap();
         q.add_message(create_message(11, vec![4, 5])).unwrap();
 
@@ -251,7 +395,7 @@ mod tests {
 
     #[test]
     fn add_message_rejects_duplicate_seq_no() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(4, vec![])).unwrap();
 
         let err = q.add_message(create_message(4, vec![])).unwrap_err();
@@ -262,7 +406,7 @@ mod tests {
 
     #[test]
     fn add_message_rejects_out_of_order_seq_no() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(10, vec![])).unwrap();
 
         let err = q.add_message(create_message(7, vec![])).unwrap_err();
@@ -273,7 +417,7 @@ mod tests {
 
     #[test]
     fn add_message_rejects_when_queue_closed_for_new_messages() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.close_for_new_messages();
 
         let err = q.add_message(create_message(1, vec![])).unwrap_err();
@@ -283,7 +427,7 @@ mod tests {
 
     #[test]
     fn get_messages_to_send_returns_all_and_advances_last_sent_index() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(3, vec![10])).unwrap();
         q.add_message(create_message(4, vec![20])).unwrap();
 
@@ -301,34 +445,34 @@ mod tests {
 
     #[test]
     fn get_messages_to_send_empty_queue_returns_empty() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         let msgs = q.get_messages_to_send().unwrap();
         assert!(msgs.is_empty());
     }
 
     #[test]
     fn get_messages_to_send_with_length_threshold_not_enough_messages() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(1, vec![])).unwrap();
 
         let result = q.get_messages_to_send_with_length_threshold(2);
 
         match &result {
-            GetMessagesToSendWithLengthThresholdResult::NotEnoughMessages => {}
+            GetMessagesToSendResult::NotEnoughMessages => {}
             _ => panic!("expected NotEnoughMessages, got {:?}", result),
         }
     }
 
     #[test]
     fn get_messages_to_send_with_length_threshold_ok_when_at_threshold() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(11, vec![])).unwrap();
         q.add_message(create_message(12, vec![])).unwrap();
 
         let result = q.get_messages_to_send_with_length_threshold(2);
 
         match &result {
-            GetMessagesToSendWithLengthThresholdResult::Ok(msgs) => {
+            GetMessagesToSendResult::Ok(msgs) => {
                 assert_eq!(msgs.len(), 2);
                 assert_eq!(msgs[0].seq_no, 11);
                 assert_eq!(msgs[1].seq_no, 12);
@@ -339,7 +483,7 @@ mod tests {
 
     #[test]
     fn get_messages_to_send_with_length_threshold_ok_when_above_threshold() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(1, vec![])).unwrap();
         q.add_message(create_message(2, vec![])).unwrap();
         q.add_message(create_message(3, vec![])).unwrap();
@@ -347,14 +491,14 @@ mod tests {
         let result = q.get_messages_to_send_with_length_threshold(2);
 
         match &result {
-            GetMessagesToSendWithLengthThresholdResult::Ok(msgs) => assert_eq!(msgs.len(), 3),
+            GetMessagesToSendResult::Ok(msgs) => assert_eq!(msgs.len(), 3),
             other => panic!("expected Ok(...), got {:?}", other),
         }
     }
 
     #[test]
     fn acknowledge_message_removes_front_when_seq_no_matches() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(5, vec![])).unwrap();
         q.add_message(create_message(6, vec![])).unwrap();
         q.add_message(create_message(7, vec![])).unwrap();
@@ -371,7 +515,7 @@ mod tests {
 
     #[test]
     fn acknowledge_message_returns_error_when_empty() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
 
         let err = q.acknowledge_message(8).unwrap_err();
 
@@ -381,7 +525,7 @@ mod tests {
 
     #[test]
     fn acknowledge_message_errors_when_seq_no_mismatch() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(1, vec![])).unwrap();
         let _ = q.get_messages_to_send().unwrap();
 
@@ -393,7 +537,7 @@ mod tests {
 
     #[test]
     fn reset_progress_clears_last_sent_so_messages_can_be_resent() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(8, vec![])).unwrap();
         q.add_message(create_message(9, vec![])).unwrap();
 
@@ -417,7 +561,7 @@ mod tests {
 
     #[test]
     fn close_for_new_messages_prevents_further_adds() {
-        let mut q = MessageQueue::new();
+        let mut q = create_queue();
         q.add_message(create_message(1, vec![])).unwrap();
 
         q.close_for_new_messages();
