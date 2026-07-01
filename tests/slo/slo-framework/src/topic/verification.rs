977fa18da5465af18c076ef84f3dad4f848b9c54
aral01 <mr.aral01@yandex.ru>
2026-07-01 10:34:44 +0300

    Remove `dash_map` dependency

--- tests/slo/slo-framework/src/topic/verification.rs
+++ tests/slo/slo-framework/src/topic/verification.rs
@@ -1,4 +1,5 @@
-use dashmap::DashMap;
+use std::collections::{hash_map, HashMap};
+use std::sync::Mutex;
 
 type PartitionID = i64;
 type ProducerID = String;
@@ -9,8 +10,8 @@ type MessageKey = (ProducerID, ydb::PartitionSessionKey);
 
 #[derive(Default)]
 pub(super) struct MessagesOrder {
-    per_stream: DashMap<MessageKey, SeqNo>,
-    per_producer: DashMap<ProducerID, SeqNo>,
+    per_stream: Mutex<HashMap<MessageKey, SeqNo>>,
+    per_producer: Mutex<HashMap<ProducerID, SeqNo>>,
 }
 
 fn message_into_key_value(message: &ydb::TopicReaderMessage) -> (MessageKey, SeqNo) {
@@ -28,44 +29,48 @@ impl MessagesOrder {
         let (key, seq_no) = message_into_key_value(message);
         let producer_id = message.get_producer_id().to_string();
 
-        match self.per_producer.entry(producer_id) {
-            dashmap::Entry::Occupied(mut occupied_entry) => {
-                let last_seq_no = occupied_entry.get_mut();
+        {
+            let mut per_producer = self.per_producer.lock().unwrap();
+            match per_producer.entry(producer_id) {
+                hash_map::Entry::Occupied(mut e) => {
+                    let last_seq_no = e.get_mut();
 
-                if seq_no == *last_seq_no + 1 {
-                    *last_seq_no = seq_no;
-                } else if seq_no > *last_seq_no {
-                    return Err(format!(
-                        "messages seq_no violated: last seen seq_no: {}, current seq_no: {seq_no}",
-                        *last_seq_no
-                    ));
+                    if seq_no == *last_seq_no + 1 {
+                        *last_seq_no = seq_no;
+                    } else if seq_no > *last_seq_no {
+                        return Err(format!(
+                            "messages seq_no violated: last seen seq_no: {}, current seq_no: {seq_no}",
+                            *last_seq_no
+                        ));
+                    }
                 }
-            }
-
-            dashmap::Entry::Vacant(vacant_entry) => {
-                if seq_no != 1 {
-                    return Err(format!(
-                        "messages seq_no violated: starting seq_no {seq_no}"
-                    ));
+                hash_map::Entry::Vacant(e) => {
+                    if seq_no != 1 {
+                        return Err(format!(
+                            "messages seq_no violated: starting seq_no {seq_no}"
+                        ));
+                    }
+                    e.insert(seq_no);
                 }
-
-                vacant_entry.insert(seq_no);
             }
         }
 
-        match self.per_stream.entry(key) {
-            dashmap::Entry::Occupied(mut e) => {
-                let prev = *e.get();
-                if prev + 1 != seq_no {
-                    return Err(format!(
-                        "messages seq_no order violated: expected {}, got {seq_no}",
-                        prev + 1
-                    ));
+        {
+            let mut per_stream = self.per_stream.lock().unwrap();
+            match per_stream.entry(key) {
+                hash_map::Entry::Occupied(mut e) => {
+                    let prev = *e.get();
+                    if prev + 1 != seq_no {
+                        return Err(format!(
+                            "messages seq_no order violated: expected {}, got {seq_no}",
+                            prev + 1
+                        ));
+                    }
+                    *e.get_mut() = seq_no;
                 }
-                *e.get_mut() = seq_no;
-            }
-            dashmap::Entry::Vacant(e) => {
-                e.insert(seq_no);
+                hash_map::Entry::Vacant(e) => {
+                    e.insert(seq_no);
+                }
             }
         }
 
@@ -75,14 +80,15 @@ impl MessagesOrder {
 
 #[derive(Default)]
 pub(super) struct OffsetOrder {
-    inner: DashMap<PartitionID, Offset>,
+    inner: Mutex<HashMap<PartitionID, Offset>>,
 }
 
 impl OffsetOrder {
     pub(super) fn ack_message(&self, message: &ydb::TopicReaderMessage) -> Result<(), String> {
         let partition_id = message.get_partition_id();
+        let inner = self.inner.lock().unwrap();
 
-        match self.inner.get(&partition_id) {
+        match inner.get(&partition_id) {
             Some(current_ack_end_offset) if message.offset < *current_ack_end_offset => {
                 Err(format!(
                     "partition: {partition_id}, offset: {}, already committed end_offset: {})",
@@ -96,6 +102,8 @@ impl OffsetOrder {
 
     pub(super) fn insert(&self, partition_id: PartitionID, end_offset: Offset) {
         self.inner
+            .lock()
+            .unwrap()
             .entry(partition_id)
             .and_modify(|current_ack_end_offset| {
                 *current_ack_end_offset = (*current_ack_end_offset).max(end_offset);
