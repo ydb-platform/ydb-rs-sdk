22fc8e4ddb1b24c14763d56aaf261e413ea9adf9
Gleb Smirnov <glebsmirnov0708@gmail.com>
2026-07-22 13:50:31 +0300

    Add real-world example for `closure!` macro

--- ydb/src/async_closure/mod.rs
+++ ydb/src/async_closure/mod.rs
@@ -117,41 +117,40 @@ where
 ///
 /// # Usage example
 ///
-/// ```
-/// # use ydb::{
-/// #     closure,
-/// #     async_closure::{
-/// #         AsyncFnMut,
-/// #         with_lifetime::Mut,
-/// #     }
-/// # };
-/// # struct Action;
+/// ```no_run
+/// # use ydb::{closure, Transaction};
 /// #
-/// # impl Action {
-/// #     fn get() -> Self { Self }
-/// #     async fn perform(&mut self, logs: &mut Vec<String>) -> Result<(), ()> {
-/// #         Ok(())
-/// #     }
+/// # enum Withdraw {
+/// #     Done { remaining: i64 },
+/// #     Insufficient,
 /// # }
 /// #
-/// async fn retry<F: AsyncFnMut<Mut<Action>, Output = bool>>(mut attempt: F) {
-///     let mut action = Action::get();
+/// # #[tokio::main]
+/// # async fn main() {
+/// #   let qc: ydb::QueryClient = todo!();
+/// #   let user_id: i64 = 1;
+///     qc
+///         .retry_tx(closure!(async |tx: &mut Transaction| {
+///             let mut row = tx
+///                 .query_row("SELECT balance FROM accounts WHERE id = $id")
+///                 .param("$id", user_id)
+///                 .await?;
+///             let balance: i64 = row.remove_field_by_name("balance")?.try_into()?;
 ///
-///     while !attempt.call(&mut action).await {
-///         println!("Failed, trying again...");
-///     }
-/// }
+///             if balance < 100 {
+///                 tx.rollback().await?;
+///                 return Ok(Withdraw::Insufficient);
+///             }
 ///
-/// #[tokio::main]
-/// async fn main() {
-///     let mut logs = vec![];
-///     retry(closure!(
-///         [&mut logs],
-///         async |action: &mut Action| {
-///             action.perform(logs).await.is_ok()
-///         }
-///     )).await;
-/// }
+///             tx.exec("UPDATE accounts SET balance = balance - 100 WHERE id = $id")
+///                 .param("$d", user_id)
+///                 .await?;
+///             Ok(Withdraw::Done {
+///                 remaining: balance - 100,
+///             })
+///         }))
+///         .await;
+/// # }
 /// ```
 #[macro_export]
 macro_rules! closure {
