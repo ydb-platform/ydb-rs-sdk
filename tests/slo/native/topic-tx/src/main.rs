e3bd7015a04bad6798935d1d01ead86651f893a1
Rinat Shigapov <rinatshigapov@gmail.com>
2026-08-17 19:40:42 +0800

    feat(slo-framework): slo-framework uses ring crypto provider

--- tests/slo/native/topic-tx/src/main.rs
+++ tests/slo/native/topic-tx/src/main.rs
@@ -3,9 +3,10 @@
 mod storage;
 mod workload;
 
-use slo_framework::run;
+use slo_framework::{install_ring_crypto_provider, run};
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
+    install_ring_crypto_provider();
     run(|fw| Box::pin(workload::new_workload(fw.clone()))).await
 }
--- tests/slo/native/topic/src/main.rs
+++ tests/slo/native/topic/src/main.rs
@@ -1,9 +1,10 @@
 #![recursion_limit = "256"]
 mod storage;
 
-use slo_framework::run;
+use slo_framework::{install_ring_crypto_provider, run};
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
+    install_ring_crypto_provider();
     run(|fw| Box::pin(storage::new_workload(fw.clone()))).await
 }
